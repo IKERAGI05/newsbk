13d076addec978e3a06614548717c84763048eac
Iker Aguinaga <iaguinaga@ayesa.com>
2025-04-25 13:28:04 +0200

    V1.0

---
+++ src/main/java/com/newsbk/newsbk/NewsbkApplication.java
@@ -0,0 +1,13 @@
+package com.newsbk.newsbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsbkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsbk/newsbk/NewsbkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsbk.newsbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsbkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
